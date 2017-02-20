35e3f4e22a6883c9bf0e25ac0836333e1dde4dc9
noobiehacker <davidkwokhochan@gmail.com>
2017-02-20 12:40:49 -0800

    Initial Commit

---
+++ boxECM/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  boxECM
+//
+//  Created by davix on 2/20/17.
+//  Copyright © 2017 revasolutions. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
